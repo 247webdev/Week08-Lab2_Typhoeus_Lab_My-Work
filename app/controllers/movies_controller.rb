2798b3e80f7c9e3db44885796c7bb60277e0ac4d
Christian Chandler <247webdev+github@googlemail.com>
2015-03-03 14:14:35 -0800

    part 1 working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,19 @@
 class MoviesController < ApplicationController
   def index
+
   end
 
-  def show
-  end
-
-  def new
-  end
-
-  def edit
+  def search
+    search_title = params[:search]
+    request = Typhoeus::Request.new(
+      "http://www.omdbapi.com/?",
+      method: :get,
+      params: { s: search_title }
+    )
+    request.run
+    movies = JSON.parse(request.response.body)
+    @movies = movies["Search"]
+    # binding.pry
+    render :index
   end
 end
